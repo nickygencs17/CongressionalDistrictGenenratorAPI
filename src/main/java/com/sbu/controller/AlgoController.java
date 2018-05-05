c9fd4872337610f63b955fedbe37e5e13be2b2a8
nickygencs17 <nick_genco@hotmail.com>
2018-05-04 21:45:53 -0400

    hey how ya doing

--- src/main/java/com/sbu/controller/AlgoController.java
+++ src/main/java/com/sbu/controller/AlgoController.java
@@ -2,11 +2,13 @@ package com.sbu.controller;
 
 import com.sbu.data.AppUserRepository;
 import com.sbu.data.entitys.AppUser;
+import com.sbu.data.entitys.StartAlgoObject;
 import com.sbu.data.entitys.Update;
 import com.sbu.data.entitys.UsState;
 import com.sbu.services.AlgoService;
 import com.sbu.services.AppUserService;
 import com.sbu.services.StateService;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -36,22 +38,19 @@ public class AlgoController {
     int ccoefficient;
     int fcoefficient;
 
-    @RequestMapping(method = RequestMethod.GET)
-    Response getStartAlgo(@RequestParam("state") String stateName,
-                          @RequestParam("populationDeviation") float populationDeviation,
-                          @RequestParam("ccoefficient") int ccoefficient,
-                          @RequestParam("fcoefficient") int fcoefficient) {
-
+    @RequestMapping(method = RequestMethod.POST)
+    Response getStartAlgo(@RequestBody StartAlgoObject startAlgoObject) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
         AppUser appUser = (AppUser) appUserService.getUserByUsername(username);
-        appUser.setPopulation_coefficient(populationDeviation);
-        appUser.setCompactness_coefficient(ccoefficient);
-        appUser.setFairness_coefficient(fcoefficient);
+        appUser.setPopulation_coefficient(startAlgoObject.getPopulation_deviation());
+        appUser.setCompactness_coefficient(startAlgoObject.getC_coefficient());
+        appUser.setFairness_coefficient(startAlgoObject.getF_coefficient());
         appUserRepository.save(appUser);
-        this.populationDeviation = populationDeviation;
-        this.ccoefficient = ccoefficient;
-        this.fcoefficient = fcoefficient;
-        selectedState = stateService.getStatebyId(stateName);
+        this.populationDeviation = startAlgoObject.getPopulation_deviation();
+        this.ccoefficient = startAlgoObject.getC_coefficient();
+        this.fcoefficient = startAlgoObject.getF_coefficient();
+        selectedState = stateService.getStatebyId(startAlgoObject.getState_id());
         return getUpdate();
     }
 
--- src/main/java/com/sbu/data/entitys/StartAlgoObject.java
+++ src/main/java/com/sbu/data/entitys/StartAlgoObject.java
@@ -1,4 +1,73 @@
 package com.sbu.data.entitys;
 
+import java.util.List;
+
 public class StartAlgoObject {
+
+    List<String> excluded_precinct_ids;
+    List<String> included_districts_ids;
+    float population_deviation;
+    int c_coefficient;
+    int f_coefficient;
+    String state_id;
+
+    public StartAlgoObject() {
+    }
+
+    public StartAlgoObject(List<String> excluded_precinct_ids, List<String> included_districts_ids, float population_deviation, int c_coefficient, int f_coefficient, String state_id) {
+        this.excluded_precinct_ids = excluded_precinct_ids;
+        this.included_districts_ids = included_districts_ids;
+        this.population_deviation = population_deviation;
+        this.c_coefficient = c_coefficient;
+        this.f_coefficient = f_coefficient;
+        this.state_id = state_id;
+    }
+
+    public List<String> getExcluded_precinct_ids() {
+        return excluded_precinct_ids;
+    }
+
+    public void setExcluded_precinct_ids(List<String> excluded_precinct_ids) {
+        this.excluded_precinct_ids = excluded_precinct_ids;
+    }
+
+    public List<String> getIncluded_districts_ids() {
+        return included_districts_ids;
+    }
+
+    public void setIncluded_districts_ids(List<String> included_districts_ids) {
+        this.included_districts_ids = included_districts_ids;
+    }
+
+    public float getPopulation_deviation() {
+        return population_deviation;
+    }
+
+    public void setPopulation_deviation(float population_deviation) {
+        this.population_deviation = population_deviation;
+    }
+
+    public int getC_coefficient() {
+        return c_coefficient;
+    }
+
+    public void setC_coefficient(int c_coefficient) {
+        this.c_coefficient = c_coefficient;
+    }
+
+    public int getF_coefficient() {
+        return f_coefficient;
+    }
+
+    public void setF_coefficient(int f_coefficient) {
+        this.f_coefficient = f_coefficient;
+    }
+
+    public String getState_id() {
+        return state_id;
+    }
+
+    public void setState_id(String state_id) {
+        this.state_id = state_id;
+    }
 }
